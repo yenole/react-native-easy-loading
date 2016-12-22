3ddf1808f58eb85b7e7e958479f0052bcef007e1
Yenole <Netxy@vip.qq.com>
2016-12-22 18:29:53 +0800

    update

--- index.js
+++ index.js
@@ -48,26 +48,22 @@ export class Loading extends React.Component {
         clearTimeout(this.handle);
     }
     render() {
-        if (this.state.isShow) {
-            clearTimeout(this.handle);
-            (this.state.timeout != -1) && (this.handle = setTimeout(() => {
-                EasyLoading.dismis(this.props.type || 'default');
-            }, this.state.timeout));
-            return (
-                <Modal
-                    animationType={"fade"}
-                    transparent={true}
-                    visible={this.state.isShow}
-                    onRequestClose={() => { alert("Modal has been closed.") } }>
-                    <View style={[this.props.loadStyle, styles.load_box]}>
-                        <ActivityIndicator animating={true} color={this.props.color || '#FFF'} size={'large'} style={styles.load_progress} />
-                        <Text style={[styles.load_text, this.props.textStyle]}>{this.state.text}</Text>
-                    </View>
-                </Modal>
-            );
-        } else {
-            return (<View />)
-        }
+        clearTimeout(this.handle);
+        (this.state.timeout != -1) && (this.handle = setTimeout(() => {
+            EasyLoading.dismis(this.props.type || 'default');
+        }, this.state.timeout));
+        return (
+            <Modal
+                animationType={"fade"}
+                transparent={true}
+                visible={this.state.isShow}
+                onRequestClose={() => { alert("Modal has been closed.") } }>
+                <View style={[this.props.loadingStyle, styles.load_box]}>
+                    <ActivityIndicator animating={true} color={this.props.color || '#FFF'} size={'large'} style={styles.load_progress} />
+                    <Text style={[styles.load_text, this.props.textStyle]}>{this.state.text}</Text>
+                </View>
+            </Modal>
+        );
     }
 }
 
